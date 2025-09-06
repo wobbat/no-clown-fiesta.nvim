726a0e0914b15ccb96a6099f425c37962e95eaaa
wobbat <mail@wobbat.com>
2025-09-06 23:33:16 +0200

    Update colors for lualine theme states

--- lua/lualine/themes/no-clown-fiesta.lua
+++ lua/lualine/themes/no-clown-fiesta.lua
@@ -4,16 +4,19 @@ local colors = {
   bg = "#171717",
   fg = "#D0D0D0",
   gray = "#373737",
+  red = "#b46958", 
+  green = "#88afa2", 
+  blue = "#BAD7FF",
 }
 
 return {
   normal = {
-    a = { fg = colors.fg, bg = colors.bg, gui = "bold" },
+    a = { fg = colors.fg, bg = colors.blue, gui = "bold" },
     b = { fg = colors.fg, bg = colors.bg },
     c = { fg = colors.fg, bg = colors.bg },
   },
-  insert = { a = { fg = colors.fg, bg = colors.bg, gui = "bold" } },
-  visual = { a = { fg = colors.fg, bg = colors.bg, gui = "bold" } },
+  insert = { a = { fg = colors.fg, bg = colors.green, gui = "bold" } },
+  visual = { a = { fg = colors.fg, bg = colors.red, gui = "bold" } },
   command = { a = { fg = colors.fg, bg = colors.bg, gui = "bold" } },
   replace = { a = { fg = colors.fg, bg = colors.bg, gui = "bold" } },
   inactive = {
